d13a50be994a6cbdd976e0633c90b393ca3d0301
Яков <yakov.norev@gmail.com>
2018-01-18 23:42:11 +0700

    mysql

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ var server = http.createServer(function (req, res) {
 	res.end('Hello Http');
 });
 
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "yourusername",
+	password: "yourpassword"
+});
+
+con.connect(function (err) {
+	if (err) throw err;
+	console.log("Connected!");
+});
 server.listen(18080);
