5d186d43e50e209d5393d9db9242d8250da8703c
Яков <yakov.norev@gmail.com>
2018-01-20 14:22:06 +0700

    1

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 console.log("-");
+
+var mysql = require('mysql');
+var config = {};
+config.host = 'localhost';
+config.user = 'user1';
+config.password = '123';
+config.database = 'db1';
+
+var connection = mysql.createConnection(config);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+	if (error) {}
+	console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
 	res.writeHead(200);
 	res.end('Hello Http');
 });
-
-var mysql = require('mysql');
-
-var client = mysql.createClient();
-client.host = '127.0.0.1';
-client.port = '3306';
-client.user = 'someuser';
-client.password = 'userpass';
-client.database = 'node';
-
-client.query('SELECT * FROM users', function (error, result, fields) {
-	console.log(result);
-});
-
-con.connect(function (err) {
-	if (err) throw err;
-	console.log("Connected!");
-});
-server.listen(18080);
+//server.listen(18080);
